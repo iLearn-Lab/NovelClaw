3d8436604fa42b6bb715676ff65914ec3ffbe7e8
夏雨 <131525733+sirvffg@users.noreply.github.com>
2026-03-25 18:44:31 +0800

    Delete start_local.ps1

--- start_local.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [string]$BindHost = "127.0.0.1",
-  [int]$Port = 8010,
-  [switch]$Reload
-)
-
-$ErrorActionPreference = "Stop"
-
-& (Join-Path $PSScriptRoot "local_web_portal\start_local.ps1") -BindHost $BindHost -Port $Port -Reload:$Reload
